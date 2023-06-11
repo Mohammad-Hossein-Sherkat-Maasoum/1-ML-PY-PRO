bd012428a880fcfd22bbce3217e7416dc7bd5c1b
AmirCollider <101126826+Mohammad-Hossein-Sherkat-Maasoum@users.noreply.github.com>
2023-06-11 19:45:42 +0330

    Add files via upload

---
+++ 1ML.py
@@ -0,0 +1,16 @@
+# First ML pro // AmirCollider
+import numpy as np
+import pandas as pd
+import sklearn
+
+# ایجاد یک آرایه یک بعدی[One-dimensional array]
+a = np.array([1, 2, 3, 4, 5])
+
+# ایجاد یک آرایه دو بعدی[Two-dimensional array]
+b = np.array([[1, 2, 3], [4, 5, 6]])
+
+print (a[0])
+
+print(b[0,0])
+
+print(b[1,0])
